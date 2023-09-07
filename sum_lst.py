1fe5dc36ae4b17c50323e8106c9e9fcb1a2c30da
VictorHSandi <Victor.H.Sandi-1@ou.edu>
2023-09-07 14:35:19 -0500

    Added mul_lst to sum_lst.py

--- sum_lst.py
+++ sum_lst.py
@@ -8,5 +8,21 @@
 def sum_lst(lst):
     return sum(lst)
 
-print(sum_lst([1,1,1]))
+# print(sum_lst([1,1,1]))
 
+# Extend the software program to add a function / method which computes the product (multiplication)
+# of all of a list / array of numbers given as a parameter and returns the result. Commit the result. The
+# new function / method may be in the same file as part 1’s function / method, or a new file, but this
+# will be a second commit to the repository
+
+def mul_lst(lst):
+    if len(lst) == 0:
+        return 0
+    total = 1
+    for x in lst:
+        total = x * total
+
+    return total
+
+
+# print(mul_lst([1,2,3]))
