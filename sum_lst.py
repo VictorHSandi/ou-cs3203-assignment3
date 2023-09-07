9baaef3d8a8359d2425c357c067ea82e24301980
VictorHSandi <Victor.H.Sandi-1@ou.edu>
2023-09-07 14:30:20 -0500

    Added sum_lst to sum_lst.py

---
+++ sum_lst.py
@@ -0,0 +1,12 @@
+# Create a software program (in any language you wish) which contains a function / method which
+# computes the sum of a list / array of numbers (integers, whatever you want) given as a parameter and
+# returns the result. Commit the resulting file(s). If your language is a compiled language (i.e., one in
+# which the source code is compiled into a different, binary format) then only commit the source code
+# file.
+
+
+def sum_lst(lst):
+    return sum(lst)
+
+print(sum_lst([1,1,1]))
+
